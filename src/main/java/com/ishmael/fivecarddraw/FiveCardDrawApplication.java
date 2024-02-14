6830ab6749cf23be6da842ccf45b760f26776bc2
Ishmael Sibiya <ishmael.sibiya@fnb.co.za>
2024-02-14 18:23:44 +0200

    initial commit

---
+++ src/main/java/com/ishmael/fivecarddraw/FiveCardDrawApplication.java
@@ -0,0 +1,15 @@
+package com.ishmael.fivecarddraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveCardDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveCardDrawApplication.class, args);
+
+
+	}
+
+}
